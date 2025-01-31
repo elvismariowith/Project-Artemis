afd3abe5536275fa7703528cca237087fe2485f8
luciano093 <luparedes100@gmail.com>
2025-01-30 20:45:57 -0500

    Add debug information and fixes for controller program

--- Controller Input/controller.cpp
+++ Controller Input/controller.cpp
@@ -1,34 +1,62 @@
+#include <SDL2/SDL.h>
 #include <SDL2/SDL_gamecontroller.h>
 #include <iostream>
 
 int main() {
-    int joystick_index = SDL_NumJoysticks();
-
-    if (joystick_index < 1) {
-        std::cout<<"No joysticks available"<<std::endl;
+    if ((SDL_Init(SDL_INIT_GAMECONTROLLER) == -1)) { 
+        std::cout<<"Could not initialize SDL: "<<SDL_GetError()<<std::endl;
         return -1;
     }
 
-    if (!SDL_IsGameController(joystick_index)) {
-    std::cout<<"Joystick not supported"<<std::endl;
-    return -1;
+    std::cout<<"SDL initialized"<<std::endl;
+
+    if (SDL_NumJoysticks() < 1) {
+        std::cout<<"No joysticks available"<<std::endl;
+        SDL_Quit();
+        return -1;
     }
 
-    SDL_GameController* controller = SDL_GameControllerOpen(joystick_index);
+    std::cout<<"Number of controllers: "<<SDL_NumJoysticks()<<std::endl;
+
+    if (!SDL_IsGameController(0)) {
+        std::cout<<"Joystick not supported"<<std::endl;
+        return -1;
+    }
+
+    SDL_GameController* controller = SDL_GameControllerOpen(0);
 
     if (controller == nullptr) {
         std::cout<<"Error opening controller"<<std::endl;
         return -1;
     }
 
-    SDL_GameControllerAxis axis = SDL_CONTROLLER_AXIS_LEFTX;
+    std::cout<<"Using "<<SDL_GameControllerName(controller)<<std::endl;
+
+    char guid[33];
+    SDL_JoystickGetGUIDString(SDL_JoystickGetGUID(SDL_GameControllerGetJoystick(controller)), guid, sizeof(guid));
+    std::cout << "Controller GUID: " << guid << std::endl;
 
     while (true) {
         int right_x = SDL_GameControllerGetAxis(controller, SDL_CONTROLLER_AXIS_RIGHTX);
         int left_x = SDL_GameControllerGetAxis(controller, SDL_CONTROLLER_AXIS_LEFTX);
+
         int right_y = SDL_GameControllerGetAxis(controller, SDL_CONTROLLER_AXIS_RIGHTY);
         int left_y = SDL_GameControllerGetAxis(controller, SDL_CONTROLLER_AXIS_LEFTY);
+
+        // for debuggin purposes
+        if (!(right_x == 0 && left_x == 0 && right_y == 0 && left_y == 0)) {
+            std::cout<<"x: ("<<right_x<<", "<<left_x<<")"<<std::endl;
+            std::cout<<"y: ("<<right_y<<", "<<left_y<<")"<<std::endl;
+        }
+
+        if (SDL_GetError()) {
+            std::cout<<SDL_GetError()<<std::endl;
+            return -1;
+        }
     }
+
+    SDL_GameControllerClose(controller);
+    SDL_Quit();
 
     return 0;
 }
