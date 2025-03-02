4ac84c5a4123d3ac0069ec34e3eb1a83923f41c1
luciano093 <luparedes100@gmail.com>
2025-03-01 19:09:19 -0500

    Add real servo file

---
+++ src/Controller Input/servo V3.ino
@@ -0,0 +1,47 @@
+  #include <Servo.h>
+
+Servo myservo;
+int default_position = 90;
+int currentposition = default_position;
+
+int analogPin = A0;
+
+int mapRange(int input){
+  return map(input, 0, 677, 0, 180); //Serial.println(charValue)// 1st pair: joystick // 2nd pair: servo 
+}
+void setup() {
+  Serial.begin(9600); // communication with PS4
+  myservo.attach(10);
+  myservo.write(default_position);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:  
+
+  //read joystick
+  if (Serial.available()){
+    int angle = Serial.parseInt();
+
+    Serial.println(angle);
+
+    if (angle == -1){
+      if (currentposition != 0){
+        currentposition = currentposition - 5;
+        myservo.write(currentposition);
+      }
+    }
+
+    if (angle == 1){
+      if (currentposition != 180){
+        currentposition = currentposition + 5;
+        myservo.write(currentposition);
+      }
+      
+    }
+
+    //Serial.print("Current position: ");
+    Serial.println(angle);
+    
+  } 
+  
+}
