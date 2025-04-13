b1b8b1df32b2389be4f73a616c52f46fb6e46683
luciano093 <luparedes100@gmail.com>
2025-04-13 13:56:40 -0400

    Implement Y direction for pin 11

--- src/Controller Input/servo/servo.ino
+++ src/Controller Input/servo/servo.ino
@@ -1,8 +1,12 @@
-  #include <Servo.h>
+#include <Servo.h>
 
 Servo myservo;
+Servo myservoY;
 int default_position = 90;
+int default_positionY = 90;
 int currentposition = default_position;
+int currentpositionY = default_positionY;
+
 
 int analogPin = A0;
 
@@ -13,6 +17,9 @@ void setup() {
   Serial.begin(9600); // communication with PS4
   myservo.attach(10);
   myservo.write(default_position);
+
+  myservoY.attach(11);
+  myservoY.write(default_positionY);
 }
 
 void loop() {
@@ -26,15 +33,30 @@ void loop() {
 
     if (angle == -1){
       if (currentposition != 0){
-        currentposition = currentposition - 5;
+        currentposition = currentposition - 2;
         myservo.write(currentposition);
       }
     }
 
     if (angle == 1){
       if (currentposition != 180){
-        currentposition = currentposition + 5;
+        currentposition = currentposition + 2;
         myservo.write(currentposition);
+      }
+      
+    }
+
+    if (angle == -2){
+      if (currentpositionY != 0){
+        currentpositionY = currentpositionY - 2;
+        myservoY.write(currentpositionY);
+      }
+    }
+
+    if (angle == 2){
+      if (currentpositionY != 180){
+        currentpositionY = currentpositionY + 2;
+        myservoY.write(currentpositionY);
       }
       
     }
